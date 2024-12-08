8b447ccc2e8390652a1faea5f6ff365ed8fb2ecc
SpyC0der77 <carter.stach@gmail.com>
2024-12-08 15:03:28 -0500

    fixed for realzies

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,7 @@ export default function RootLayout({
           <HeartsModal />
           <PracticeModal />
           {children}
+          <Analytics/>
         </body>
       </html>
     </ClerkProvider>
